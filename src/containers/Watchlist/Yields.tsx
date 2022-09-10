8d29f4dcc856d808b8650e14415d7cfae76b8ca6
orlando <naijarace1@gmail.com>
2022-09-10 23:12:09 +0100

    Add files via upload

---
+++ src/containers/Watchlist/Yields.tsx
@@ -0,0 +1,89 @@
+import { useMemo } from 'react'
+import { FolderPlus, Trash2 } from 'react-feather'
+import styled from 'styled-components'
+import { Header, TYPE } from '~/Theme'
+import { Panel } from '~/components'
+import Row from '~/components/Row'
+import { Menu } from '~/components/DropdownMenu'
+import { columns, TableWrapper } from '~/components/YieldsPage/shared'
+import { YieldsSearch } from '~/components/Search'
+import { useIsClient } from '~/hooks'
+import { DEFAULT_PORTFOLIO_NAME, useWatchlist } from '~/contexts/LocalStorage'
+
+interface IFolder {
+	isSaved?: boolean
+}
+
+const Action = styled.button<IFolder>`
+	svg {
+		fill: ${({ theme: { text1 }, isSaved }) => (isSaved ? text1 : 'none')};
+
+		path,
+		line {
+			stroke: ${({ theme: { text1 } }) => text1};
+		}
+	}
+`
+
+export function YieldsWatchlistContainer({ protocolsDict }) {
+	const isClient = useIsClient()
+
+	const { addPortfolio, removePortfolio, savedProtocols, portfolios, selectedPortfolio, setSelectedPortfolio } =
+		useWatchlist()
+
+	const savedProtocolsInWatchlist = Object.values(savedProtocols)
+
+	const filteredProtocols = useMemo(() => {
+		if (isClient) {
+			const list = protocolsDict.filter((p) => savedProtocolsInWatchlist.includes(p.pool))
+			return list.map((t) => ({
+				id: t.pool,
+				pool: t.symbol,
+				projectslug: t.project,
+				project: t.projectName,
+				chains: [t.chain],
+				tvl: t.tvlUsd,
+				apy: t.apy,
+				apyBase: t.apyBase,
+				apyReward: t.apyReward,
+				rewardTokensSymbols: t.rewardTokensSymbols,
+				rewards: t.rewardTokensNames,
+				change1d: t.apyPct1D,
+				change7d: t.apyPct7D,
+				outlook: t.predictions.predictedClass,
+				confidence: t.predictions.binnedConfidence,
+				url: t.url,
+				category: t.category
+			}))
+		} else return []
+	}, [isClient, savedProtocolsInWatchlist, protocolsDict])
+
+	return (
+		<>
+			<YieldsSearch step={{ category: 'Yields', name: 'Watchlist', hideOptions: true }} />
+
+			<Header>Saved Pools</Header>
+
+			<Row sx={{ gap: '1rem', margin: '12px 0 -20px' }}>
+				<TYPE.main>Current portfolio:</TYPE.main>
+				<Menu name={selectedPortfolio} options={portfolios} onItemClick={(value) => setSelectedPortfolio(value)} />
+				<Action onClick={addPortfolio}>
+					<FolderPlus />
+				</Action>
+				{selectedPortfolio !== DEFAULT_PORTFOLIO_NAME && (
+					<Action onClick={removePortfolio}>
+						<Trash2 />
+					</Action>
+				)}
+			</Row>
+
+			{filteredProtocols.length ? (
+				<TableWrapper data={filteredProtocols} columns={columns} />
+			) : (
+				<Panel>
+					<p style={{ textAlign: 'center' }}>You have not saved any pools.</p>
+				</Panel>
+			)}
+		</>
+	)
+}
