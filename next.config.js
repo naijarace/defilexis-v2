05352cae13d2d1874a7fa5ae0e924e62be6188e9
orlando <naijarace1@gmail.com>
2022-09-10 20:45:51 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,67 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true'
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: '/home',
+				destination: '/',
+				permanent: true
+			},
+			{
+				source: '/chain/Binance',
+				destination: '/chain/BSC',
+				permanent: true
+			},
+			{
+				source: '/langs',
+				destination: '/languages',
+				permanent: true
+			},
+			{
+				source: '/yields/project/:path*',
+				destination: '/yields?project=:path*',
+				permanent: true
+			},
+			{
+				source: '/yields/token/:path*',
+				destination: '/yields?token=:path*',
+				permanent: true
+			},
+			{
+				source: '/yields/chain/:path*',
+				destination: '/yields?chain=:path*',
+				permanent: true
+			},
+			{
+				source: '/recent-noforks',
+				destination: '/recent',
+				permanent: true
+			},
+			{
+				source: '/liquidations',
+				destination: '/liquidations/eth',
+				permanent: false
+			}
+		]
+	},
+	images: {
+		domains: ['icons.llama.fi', 'assets.coingecko.com']
+	},
+	compiler: {
+		styledComponents: true
+	},
+	experimental: {
+		legacyBrowsers: false,
+		browsersListForSwc: true,
+		images: {
+			allowFutureImage: true
+		}
+	}
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
