fb0fa0efe38c2bb6105aa32ffa35af863cebf257
orlando <naijarace1@gmail.com>
2022-09-16 08:38:03 +0100

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,11 @@
 import LocalStorageContextProvider, { Updater as LocalStorageContextUpdater } from '~/contexts/LocalStorage'
 import { useAnalytics } from '~/hooks'
 import '~/Theme/globals.css'
+import '../assets/css/bootstrap-extended.css'
+import '../assets/css/bootstrap.min.css'
+import '../assets/css/icons.css'
+import '../assets/css/pace.min.css'
+import '../assets/css/app.css'
 
 function App({ Component, pageProps }) {
 	useAnalytics()
