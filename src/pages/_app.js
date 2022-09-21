4d23eece1630ab4a6fb754e89b92f02789b7b65c
orlando <naijarace1@gmail.com>
2022-09-21 14:31:44 +0100

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,30 +6,9 @@ import '../assets/css/bootstrap.min.css'
 import '../assets/css/icons.css'
 import '../assets/css/pace.min.css'
 import '../assets/css/app.css'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import * as ga from './ga'
-
 
 function App({ Component, pageProps }) {
 	useAnalytics()
-	const router = useRouter()
-
-	useEffect(() => {
-		const handleRouteChange = (url) => {
-		  ga.pageview(url)
-		}
-		//When the component is mounted, subscribe to router changes
-		//and log those page views
-		router.events.on('routeChangeComplete', handleRouteChange)
-	
-		// If the component is unmounted, unsubscribe
-		// from the event with the `off` method
-		return () => {
-		  router.events.off('routeChangeComplete', handleRouteChange)
-		}
-	  }, [router.events])
-	
 
 	return (
 		<LocalStorageContextProvider>
