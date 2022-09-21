baaeb3e986700625100fd4d8a592d6076cab35b6
orlando <naijarace1@gmail.com>
2022-09-21 15:03:43 +0100

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,15 +6,37 @@ import '../assets/css/bootstrap.min.css'
 import '../assets/css/icons.css'
 import '../assets/css/pace.min.css'
 import '../assets/css/app.css'
+import Script from 'next/script'
+import Head from 'next/head'
+
 
 function App({ Component, pageProps }) {
 	useAnalytics()
 
 	return (
+	    <>
+      <Head>
+        <title>Next.js</title>
+      </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=UA-55586655-1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-55586655-1');
+        `}
+      </Script>
+	
 		<LocalStorageContextProvider>
 			<LocalStorageContextUpdater />
 			<Component {...pageProps} />
 		</LocalStorageContextProvider>
+		</>
 	)
 }
 
