c0ad941bf021c45c75a4774dbfa4fc95573612b7
orlando <naijarace1@gmail.com>
2022-09-21 14:21:03 +0100

    Add files via upload

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,44 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
-				})
-
-			const initialProps = await Document.getInitialProps(ctx)
-
-			return {
-				...initialProps,
-				styles: [initialProps.styles, sheet.getStyleElement()]
-			}
-		} finally {
-			sheet.seal()
-		}
-	}
-
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;800&display=swap"
-						rel="stylesheet"
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
-
-export default MyDocument
